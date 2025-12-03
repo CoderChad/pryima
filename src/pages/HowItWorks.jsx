f61c75b28c36ee826500e4cc5d992bcb40c689be
Chad Thomas <biolifechad@gmail.com>
2025-12-03 02:30:23 +0000

    File changes

--- src/pages/HowItWorks.jsx
+++ src/pages/HowItWorks.jsx
@@ -1,24 +1,20 @@
 import React from "react";
-import HIWHero from "../components/how-it-works/HIWHero";
-import Step1Baseline from "../components/how-it-works/Step1Baseline";
-import Step2RealTimeData from "../components/how-it-works/Step2RealTimeData";
-import Step3PatternRecognition from "../components/how-it-works/Step3PatternRecognition";
-import Step4ContinuousRetraining from "../components/how-it-works/Step4ContinuousRetraining";
-import Step5PrecisionInsights from "../components/how-it-works/Step5PrecisionInsights";
-import Step6CompoundingIntelligence from "../components/how-it-works/Step6CompoundingIntelligence";
-import HIWClosingCTA from "../components/how-it-works/HIWClosingCTA";
+import HIWNewHero from "../components/how-it-works/HIWNewHero";
+import HIWProcessSteps from "../components/how-it-works/HIWProcessSteps";
+import HIWAIEngine from "../components/how-it-works/HIWAIEngine";
+import HIWDigitalTwin from "../components/how-it-works/HIWDigitalTwin";
+import HIWWhyItChangesEverything from "../components/how-it-works/HIWWhyItChangesEverything";
+import HIWFoundingMemberCTA from "../components/how-it-works/HIWFoundingMemberCTA";
 
 export default function HowItWorks() {
   return (
-    <div className="min-h-screen bg-[#02040A]">
-      <HIWHero />
-      <Step1Baseline />
-      <Step2RealTimeData />
-      <Step3PatternRecognition />
-      <Step4ContinuousRetraining />
-      <Step5PrecisionInsights />
-      <Step6CompoundingIntelligence />
-      <HIWClosingCTA />
+    <div className="bg-[#02040A] min-h-screen">
+      <HIWNewHero />
+      <HIWProcessSteps />
+      <HIWAIEngine />
+      <HIWDigitalTwin />
+      <HIWWhyItChangesEverything />
+      <HIWFoundingMemberCTA />
     </div>
   );
 }
