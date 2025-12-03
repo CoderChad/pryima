840970dff04c193413e5cdba5ebc528ac4cb50e9
Chad Thomas <biolifechad@gmail.com>
2025-12-03 02:15:48 +0000

    File changes

--- src/components/home/WearablesIntegration.jsx
+++ src/components/home/WearablesIntegration.jsx
@@ -7,11 +7,11 @@ export default function WearablesIntegration() {
     <section className="py-24 bg-[#050814] relative overflow-hidden">
       {/* Background Image with Overlay */}
       <div className="absolute inset-0 z-0">
-        <div className="absolute inset-0 bg-gradient-to-r from-[#050814] via-[#050814]/95 to-[#050814]/80 z-10" />
+        <div className="absolute inset-0 bg-gradient-to-r from-[#050814] via-[#050814]/80 to-transparent z-10" />
         <img 
           src="https://qtrypzzcjebvfcihiynt.supabase.co/storage/v1/object/public/base44-prod/public/69156bec7bead1484355f40e/17ec3cffd_IMG_0164.jpg" 
           alt="Wearables Background" 
-          className="w-full h-full object-cover opacity-60 mix-blend-luminosity"
+          className="w-full h-full object-cover opacity-80"
         />
       </div>
       
